e9d8b1e362d1670938da8176bca7d31a14aabdc0
Yanin <julia4212@rambler.ru>
2022-08-18 11:18:03 +0400

    Первое задание

---
+++ Домашка/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array = { 1, 0, 1, 0, 1, 0, 1, 0};
+int n = array.Length;
+int index = 0;
+
+while (index < n)
+{
+Console.WriteLine(array[index]);
+index++;
+}
